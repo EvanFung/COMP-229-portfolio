c15b67243c4eece99ca09ba51bf30bdb2ab2aebf
EvanFung <evanfungv@gmail.com>
2022-01-23 14:31:10 -0500

    add git ignore

---
+++ app.js
@@ -0,0 +1,54 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var projectRouter = require('./routes/projects');
+var contactRouter = require('./routes/contact');
+var aboutmeRouter = require('./routes/aboutme');
+var skillsRouter = require('./routes/skills');
+
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+ * router
+ */
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/projects',projectRouter);
+app.use('/contact',contactRouter);
+app.use('/about',aboutmeRouter);
+app.use('/services',skillsRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
