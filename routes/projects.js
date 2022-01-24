1750620964566bb770f0d104a9c5f0006ed6aed5
EvanFung <evanfungv@gmail.com>
2022-01-24 02:40:09 -0500

    projects page real data

--- routes/projects.js
+++ routes/projects.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+const projects = require('../utils/projects');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('projects/', { title: 'Express' });
+  console.log(projects);
+  res.render('projects/', { title: 'Express', projects });
 });
 
 module.exports = router;
---
+++ utils/projects.js
@@ -0,0 +1,35 @@
+const projects = [
+    {
+        "title":"Freelance-Matching-System-Client-Side",
+        "description":"The client side of freelancer matching system including the chat app",
+        "link":"https://github.com/EvanFung/Freelance-Matching-System-Client-Side"
+    },
+    {
+        "title":"Reactnd-project-readable",
+        "description":"This is second project from React Developer Nanodegree in Udacity.",
+        "link":"https://github.com/EvanFung/reactnd-project-readable"
+    },
+    {
+        "title":"JavaFX_ChatApp",
+        "description":"An information sharing desktop application which includes a server program and client program.",
+        "link":"https://github.com/EvanFung/JavaFX_ChatApp"
+    },
+    {
+        "title":"Fashion-recommendation-frontend",
+        "description":"A new Flutter project. Flutter SDK 1.9.1+hotfix.2",
+        "link":"https://github.com/EvanFung/fashion-recommendation-frontend"
+    },
+    {
+        "title":"Search_engine",
+        "description":"COMP4047 SEARCH ENGINE which is an assignment from Hong Kong Baptist University",
+        "link":"https://github.com/EvanFung/comp4047_search_engine"
+    },
+    {
+        "title":"Neighborhood Map project",
+        "description":"Fifth project from the Front-End Web Developer Nanodegree in Udacity",
+        "link":"https://github.com/EvanFung/FEND-project-5-neighborhood-map"
+    }
+
+]
+
+module.exports = projects;
