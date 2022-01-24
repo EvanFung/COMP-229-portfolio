620907c7e8be5dcb87dd901d25f38609f13aba5b
EvanFung <evanfungv@gmail.com>
2022-01-24 10:16:46 -0500

    image file

--- routes/projects.js
+++ routes/projects.js
@@ -3,7 +3,6 @@ var router = express.Router();
 const projects = require('../utils/projects');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  console.log(projects);
   res.render('projects/', { title: 'Express', projects });
 });
 
