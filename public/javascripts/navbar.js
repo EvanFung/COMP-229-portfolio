af08b3beeca03665896e01925ecc8ea32f152f85
EvanFung <evanfungv@gmail.com>
2022-01-28 00:21:31 -0500

    add a responsive menu

---
+++ public/javascripts/navbar.js
@@ -0,0 +1,6 @@
+(function() {
+    document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.sidenav');
+        var instances = M.Sidenav.init(elems, {});
+      });
+})()
